443f39da7e3410acd743bbfa13f1bcb1e51225f6
Webasif1 <mdasif606392@gmail.com>
2026-08-03 00:00:36 +0600

    refactor app.js to improve formatting and add sandbox start endpoint for pod and service creation

--- sandbox/server/src/app.js
+++ sandbox/server/src/app.js
@@ -1,18 +1,30 @@
 import express from "express";
 import morgan from "morgan";
-
+import { createPod } from "./kubernetes/pod.js";
+import { createService } from "./kubernetes/service";
+import { v7 as uuid } from "uuid";
 
 const app = express();
 
 app.use(morgan("dev"));
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 
-app.get("/api/sandbox/health",(req,res)=>{
+app.get("/api/sandbox/health", (req, res) => {
   res.status(200).json({
     message: "Sandbox API is healthy",
-    status:"ok"
-  })
-})
+    status: "ok",
+  });
+});
+
+app.post("/api/sandbox/start", (req, res) => {
+
+  const sandboxId = uuid();
+
+  await Promise.all([
+    createPod(sandboxId),
+    createService(sandboxId)
+  ])
+});
 
 export default app;
